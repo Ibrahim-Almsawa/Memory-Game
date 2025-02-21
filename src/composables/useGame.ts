df2ee058f0d610d8b349053d2b2ee0bc43ee99c9
Ebrahim Musawa <wwwi2021h@gmail.com>
2025-02-21 03:34:19 +0300

    update the  vue-tsc

--- src/composables/useGame.ts
+++ src/composables/useGame.ts
@@ -1,4 +1,4 @@
-import { ref, computed, watch } from 'vue';
+import { ref, computed } from 'vue';
 import type { GameState, Card, Score, Difficulty } from '../types/game';
 import { useStorage, useIntervalFn } from '@vueuse/core';
 
